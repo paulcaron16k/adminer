3f56f5a2969fefc39354f4ce380b3926a9cfdf99
Paul Caron <PaulCaron16k@Yahoo.com>
2018-06-15 05:54:00 -0400

    Include a non-default adminer.php script. If someone loads a new adminer.php from repo, it will not be used accidently.

--- htdocs/adminerWithPlugins.php
+++ htdocs/adminerWithPlugins.php
@@ -48,20 +48,26 @@ function adminer_object() {
         new AdminerServersConfig($ADMINER_SERVERS),
 
 		// Include plugins from adminer/plugin.php
-		new AdminerDatabaseHide(array('information_schema')),
-		new AdminerDumpJson,
-		new AdminerDumpBz2,
-		new AdminerDumpZip,
-		new AdminerDumpXml,
-		new AdminerDumpAlter,
-		new AdminerFileUpload(""),
-		new AdminerJsonColumn,
-		new AdminerSlugify,
-		new AdminerTranslation,
-		new AdminerForeignSystem,
-		new AdminerEnumOption,
-		new AdminerTablesFilter,
-		new AdminerEditForeign,
+		//new AdminerDatabaseHide(array('information_schema')),
+		//new AdminerDumpJson,
+		//new AdminerDumpBz2,
+		//new AdminerDumpZip,
+		//new AdminerDumpXml,
+		//new AdminerDumpAlter,
+		//new AdminerFileUpload(""),
+		//new AdminerJsonColumn,
+		//new AdminerSlugify,
+		//new AdminerTranslation,
+		//new AdminerForeignSystem,
+		//new AdminerEnumOption,
+		//new AdminerTablesFilter,
+        //new AdminerEditForeign,
+
+        //~ new AdminerSqlLog("past-" . rtrim(`git describe --tags --abbrev=0`) . ".sql"),
+        //        //~ new AdminerEditCalendar(script_src("../externals/jquery-ui/jquery-1.4.4.js") . script_src("../externals/jquery-ui/ui/jquery.ui.core.js") . script_src("../externals/jquery-ui/ui/jquery.ui.widget.js") . script_src("../externals/jquery-ui/ui/jquery.ui.datepicker.js") . script_src("../externals/jquery-ui/ui/jquery.ui.mouse.js") . script_src("../externals/jquery-ui/ui/jquery.ui.slider.js") . script_src("../externals/jquery-timepicker/jquery-ui-timepicker-addon.js") . "<link rel='stylesheet' href='../externals/jquery-ui/themes/base/jquery.ui.all.css'>\n<style>\n.ui-timepicker-div .ui-widget-header { margin-bottom: 8px; }\n.ui-timepicker-div dl { text-align: left; }\n.ui-timepicker-div dl dt { height: 25px; }\n.ui-timepicker-div dl dd { margin: -25px 0 10px 65px; }\n.ui-timepicker-div td { font-size: 90%; }\n</style>\n", "../externals/jquery-ui/ui/i18n/jquery.ui.datepicker-%s.js"),
+        //                //~ new AdminerTinymce("../externals/tinymce/jscripts/tiny_mce/tiny_mce_dev.js"),
+        //                        //~ new AdminerWymeditor(array("../externals/wymeditor/src/jquery/jquery.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.explorer.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.mozilla.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.opera.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.safari.js")),
+        //
 	);
 	
 	/* It is possible to combine customization and plugins:
@@ -74,4 +80,4 @@ function adminer_object() {
 }
 
 // include original Adminer or Adminer Editor
-include "./adminer.php";
+include "./adminerXflow.php";
